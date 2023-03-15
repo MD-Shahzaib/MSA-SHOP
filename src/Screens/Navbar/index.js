7005480c3ab10cb0560e3dfde71aec39f8b100c2
Muhammad Shahzaib <mdshahzaib36@gmail.com>
2023-03-14 21:09:17 -0700

    Create Shopping website (remainder)

--- src/Screens/Navbar/index.js
+++ src/Screens/Navbar/index.js
@@ -10,7 +10,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">REDUX</a>
+                <a className="navbar-brand" href="/">MSA-SHOP</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
