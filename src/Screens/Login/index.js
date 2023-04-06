c658d190628d409d5b1c5bc6ebe229c89ad78271
Muhammad Shahzaib <mdshahzaib36@gmail.com>
2023-04-06 02:25:04 -0700

    Create Login and register screen

--- src/Screens/Login/index.js
+++ src/Screens/Login/index.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import './login.css'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <>
             <div className="main-container">
-                <div className="bg-container">
-                    <div className='auth-box border border-white container'>
-                        <h2 className="text-white">login screen</h2>
+                <div className="bg-color d-flex justify-content-center align-items-center">
+                    <div className='container d-flex justify-content-center align-items-center'>
+                        <div className="d-flex justify-content-center flex-column bg-white rounded py-5 px-5 w-50">
+                            <h3 className='text-center mb-3'>Login</h3>
+                            <div className="form-floating mb-3">
+                                <input type="email" className="form-control" id="email" placeholder="Email" />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input type="password" className="form-control" id="password" placeholder="Password" />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                            <div className="d-flex justify-content-between flex-wrap mb-3">
+                                <div className="form-check">
+                                    <input type="checkbox" className="form-check-input" id="checkbox" />
+                                    <label className="form-check-label" htmlFor="checkbox">Remember me</label>
+                                </div>
+                                <Link to="/forgot" className='text-decoration-none'>Forgot password?</Link>
+                            </div>
+                            <button className="btn btn-primary">Login</button>
+                            <div className='text-center mt-3'>Don't have an account? <Link to="/register" className='text-decoration-none'>Register</Link></div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Screens/Register/index.js
+++ src/Screens/Register/index.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>Register</div>
+        <>
+            <div className="main-container">
+                <div className="bg-color d-flex justify-content-center align-items-center">
+                    <div className='container d-flex justify-content-center align-items-center'>
+                        <div className="d-flex justify-content-center flex-column bg-white rounded py-5 px-5 w-50">
+                            <h3 className='text-center mb-3'>Register</h3>
+                            <div className="form-floating mb-3">
+                                <input type="text" className="form-control" id="name" placeholder="name" />
+                                <label htmlFor="name">Name</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input type="tel" className="form-control" id="tel" placeholder="tel" />
+                                <label htmlFor="tel">Phone</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input type="email" className="form-control" id="email" placeholder="Email" />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input type="password" className="form-control" id="password" placeholder="Password" />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                            <button className="btn btn-primary">Register</button>
+                            <div className='text-center mt-3'>Already have an account? <Link to="/login" className='text-decoration-none'>Login</Link></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
