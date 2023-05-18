38d357d493d04f6aba89f57101d1c2c7df5a2936
Muhammad Shahzaib <mdshahzaib36@gmail.com>
2023-05-18 02:23:18 -0700

    Product fetch successfully but data not renderd fix it tommorrow

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,38 +1,37 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import data from '../../Config/Data.js'
-import './card.css'
-import '../../globalStyle.css'
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './card.css';
+// import data from '../../Config/Data.js';
+// import '../../globalStyle.css';
 
 const Card = () => {
-
-  // GET PRODUCTS FROM DATABASE
-  // useEffect(() => {
-  //   const allProducts = async () => {
-  //     try {
-  //       const response = await fetch("http://localhost:4000/product/getProducts");
-  //       const json = await response.json();
-  //       console.log("json ===> ", json);
-  //     } catch (error) {
-  //       console.log("error ===> ", error);
-  //     }
-  //   }
-  //   allProducts();
-  // }, []);
-
-  const navigate = useNavigate()
-
+  const navigate = useNavigate();
+  const [products, setProducts] = useState([]);
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch('http://localhost:4000/product/getProducts');
+      const data = await response.json();
+      setProducts(data);
+      console.log('data ==> ', data);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  };
+  console.log('products ==> ', products);
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
     <>
-      {data.map((item, index) => {
+      {products.map((item, index) => {
         return (
-          <div className="col" key={index}>
-            <div className="product-card bg-white border rounded-3" onClick={() => { navigate(`/product/${item.productId}`) }}>
+          <div className="col" key={item._id}>
+            <div className="product-card bg-white border rounded-3" onClick={() => { navigate(`/product/${item._id}`) }}>
               <div className="Product-Image-box">
                 <img src={item.productImage} alt="Product-Img" />
               </div>
               <div className="py-2 px-2">
-                <h5>{item.productTitle}</h5>
+                <h5>{item.data.title}</h5>
                 <h6 className="mt-2">{item.description.slice(0, 40)}...</h6>
                 <h5 className="mt-2">Rs : {item.price}</h5>
               </div>
